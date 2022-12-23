06a4edc9f3ddf49326db700e106d1636bc7f979b
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-12-22 16:56:04 -0800

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,31 @@
+/*Abstract in java:- Abstract in java is just showing necessary information to user and hiding its implementation from the user such process is called Abstract*/
+
+//program
+
+abstract class Animal {
+    abstract void sound();
+       public void all_animal() {
+       System.out.println("Animal sounds");
+       }
+}
+
+class Dog extends Animal {
+        void sound() {
+         System.out.println("Barking");
+        }
+}
+
+class Lion extends Animal {
+        void sound() {
+        System.out.println("Lion Roar");
+        }
+}
+
+class Break {
+public static void main(String[]args) {
+        Dog d=new Dog();
+        Lion l=new Lion();
+        d.sound();
+        l.sound();
+}
+}
