6bd16c853c7afbeaba031f51b5f36eafada848c7
Ashish Gawali <64855416+ashish200214@users.noreply.github.com>
2023-09-11 21:56:50 +0530

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,6 @@
+/* 
+  Interface :- Interface is fully un-implemented class used for declaring the set of operation of an object for developing a loosely coupled runtime polymorphic object user class to use these opeation from different subclasses of this interface.
+               It is purely abstract class allows us to define only "public static final variables" and "public abstract methods" for declaring a object operations.
+               It is basically contract between service provider and service user , service provider proides service to the service user.
+               It is introduced to achieve multiple inheritance in java.
+*/
