bec815f08a809cb9f6f604384f6d36fb13828929
Ashish Gawali <64855416+ashish200214@users.noreply.github.com>
2023-09-14 21:16:54 +0530

    Update AnonymousInnerClass.java

--- AnonymousInnerClass.java
+++ AnonymousInnerClass.java
@@ -45,7 +45,42 @@
      }
 
 
-     One more example to understand annonymous class
+     One more example to understand problem and solution
+
+     //main type 
+     interface I1{
+       void m1();
+     }
+
+     //user type
+     class Example{
+       void m2(I1 i1){  //here we have to pass the sub-class of the interface class which have m1() method implemented for execute..
+         i1.m1();       
+       }
+     }
+
+     //implemented class
+     class C implements I1{
+        @Override
+       public void m1(){
+         System.out.println("I am in C class m1 method");
+       }
+     }
+
+     class Main{
+       public static void main(String[]args){
+         Example e1 = new Example();
+         //e1.m2(new I1());   //we cannot pass the new I1() int m2() method we have to pass sub-class object which has m1() method implemented.. 
+                              //that means to implement the m1() method we need extra class so we are creating class C() for providing implementation to m1() method.
+          e1.m2(new C());                    //and by passing the object of C() class we can call m1() method
+        
+                        
+       }
+     }
+
+     //this is the problem that we requires one more implementation class to implement method to solve this problem anonymous inner class introduced.
+
+     
      
 
    
